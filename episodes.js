1f2d63f603b9485a5ee4124a42cf323d22e6ef3c
egg-lemon <ermo.lemon@gmail.com>
2026-08-28 11:23:25 +0200

    adding the files

---
+++ episodes.js
@@ -0,0 +1,326 @@
+const episodes = [
+  {
+    "id": "9H3WrAzh09A",
+    "title": "Dr. Kats; Professional Thrapist :: S01E01 :: Pot Bellied Pigs :: 1440p"
+  },
+  {
+    "id": "sNghj5ZO-I4",
+    "title": "Dr. Katz; Professional Therapist :: S01E02 :: Pretzelkins :: 1440p"
+  },
+  {
+    "id": "wFHocibrHN8",
+    "title": "Dr. Katz; Professional Therapist :: S01E03 :: Bully :: 1440p"
+  },
+  {
+    "id": "Tdg3dS3o7to",
+    "title": "Dr Katz; Professional Therapist :: S01E04 :: Cholesterol :: 1440p"
+  },
+  {
+    "id": "Br5m1cJbly0",
+    "title": "Dr. Katz; Professional Therapist :: S01E05 :: Everybody's Got a Tushy :: 1440p"
+  },
+  {
+    "id": "ZT4zdtyO1OI",
+    "title": "Dr. Katz; Professional Therapist :: S01E06 :: Family Car :: 1440p"
+  },
+  {
+    "id": "2CX-i4pktI8",
+    "title": "Dr. Katz; Professional Therapist :: S02E01 :: Bystander Ben :: 1440p"
+  },
+  {
+    "id": "YYVzA86Wo4A",
+    "title": "Dr. Katz; Professional Therapist :: S02E02 :: Real Estate :: 1440p"
+  },
+  {
+    "id": "s4NiOh87vOk",
+    "title": "Dr. Katz; Professional Therapist :: S02E03 :: Glasses :: 1080p"
+  },
+  {
+    "id": "hjtg-cIPwwc",
+    "title": "Dr. Katz; Professional Therapist :: S02E04 :: Office Management :: 1080p"
+  },
+  {
+    "id": "pwEWV-_qTgY",
+    "title": "Dr. Katz; Professional Therapist :: S02E05 :: Bees and SIDS :: 1080p"
+  },
+  {
+    "id": "i_FwDTBHF_c",
+    "title": "Dr. Katz; Professional Therapist :: S02E06 :: Drinky the Drunk Guy :: 1080p"
+  },
+  {
+    "id": "X2UpVHiI7ZQ",
+    "title": "Dr. Katz; Professional Therapist :: S02E07 :: Sticky Notes :: 1080p"
+  },
+  {
+    "id": "v3Jca9WWCu4",
+    "title": "Dr. Katz; Professional Therapist :: S02E08 :: It Takes Some Getting Used To :: 1080p"
+  },
+  {
+    "id": "YxWTn-eCHgg",
+    "title": "Dr. Katz; Professional Therapist :: S02E09 :: The Particle Board :: 1440p"
+  },
+  {
+    "id": "JV_pCswgYpc",
+    "title": "Dr. Katz; Professional Therapist :: S02E10 :: A Journey for the Betterment of People :: 1440p"
+  },
+  {
+    "id": "Ke4uxS0AOpI",
+    "title": "Dr. Katz; Professional Therapist :: S02E11 :: Theory of Intelligence :: 1440p"
+  },
+  {
+    "id": "Rs4xzQ9fCFk",
+    "title": "Dr. Katz; Professional Therapist :: S02E12 :: Henna :: 1440p"
+  },
+  {
+    "id": "ly2io7dM6d0",
+    "title": "Dr. Katz; Professional Therapist :: S02E13 :: ESP :: 1440p"
+  },
+  {
+    "id": "pKcGfq0s_D0",
+    "title": "Dr. Katz; Professional Therapist :: S03E01 :: Monte Carlo :: 1440p"
+  },
+  {
+    "id": "3O91czHur9I",
+    "title": "Dr. Katz; Professional Therapist :: S03E02 :: Blind Date :: 1440p"
+  },
+  {
+    "id": "ariaNtbwQlY",
+    "title": "Dr. Katz; Professional Therapist :: S03E03 :: Fructose :: 1440p"
+  },
+  {
+    "id": "wLgBXFlkJpY",
+    "title": "Dr. Katz; Professional Therapist :: S03E04 :: Earing :: 1440p"
+  },
+  {
+    "id": "xaFfazIG_wI",
+    "title": "Dr. Katz; Professional Therapist :: S03E05 :: Koppleman and Katz :: 1440p"
+  },
+  {
+    "id": "Sv7xvKYfO0Q",
+    "title": "Dr. Katz; Professional Therapist :: S03E06 :: Guess Who :: 1440p"
+  },
+  {
+    "id": "GbGkyvWflAY",
+    "title": "Dr. Katz; Professional Therapist :: S03E07 :: Day Planner ::1440p"
+  },
+  {
+    "id": "U2ViG_683fk",
+    "title": "Dr. Katz; Professional Therapist :: S03E08 :: Mourning Person :: 1440p"
+  },
+  {
+    "id": "2zszA85ObwU",
+    "title": "Dr. Katz; Professional Therapist :: S03E09 :: Studio Guy  :: 1440p"
+  },
+  {
+    "id": "A851DahRW8c",
+    "title": "Dr. Katz; Professional Therapist :: S03E10 :: L'il Helper :: 1440p"
+  },
+  {
+    "id": "PwNU38_TxnM",
+    "title": "Dr. Katz; Professional Therapist :: S03E11 :: Big Fat Slug :: 1440p"
+  },
+  {
+    "id": "rYA6fQkFN2c",
+    "title": "Dr. Katz; Professional Therapist :: S03E12 :: New Phone System :: 1440p"
+  },
+  {
+    "id": "vHz1sMaQIJg",
+    "title": "Dr. Katz; Professional Therapist :: S03E13 :: Reunion :: 1440p"
+  },
+  {
+    "id": "7cmGRC7fIuI",
+    "title": "Dr. Katz; Professional Therapist :: S04E01 :: Memoirs :: 1440p"
+  },
+  {
+    "id": "nxKM_gMQbqc",
+    "title": "Dr. Katz; Professional Therapist :: S04E02 :: Electric Bike :: 1440p"
+  },
+  {
+    "id": "jLUROBEg6gg",
+    "title": "Dr. Katz; Professional Therapist :: S04E03 :: Broadcaster Ben :: 1440p"
+  },
+  {
+    "id": "KEPNWDGA8Ps",
+    "title": "Dr. Katz; Professional Therapist :: S04E04 :: Trash Day :: 1440p"
+  },
+  {
+    "id": "MRHx2gEnGug",
+    "title": "Dr. Katz; Professional Therapist :: S04E05 :: Ben Treats :: 1440p"
+  },
+  {
+    "id": "YdsD5E2SzQg",
+    "title": "Dr. Katz; Professional Therapist :: S04E06 :: Sharon Meyers :: 1440p"
+  },
+  {
+    "id": "UzuemiJ3lOo",
+    "title": "Dr. Katz; Professional Therapist :: S04E07 :: Mask :: 1440p"
+  },
+  {
+    "id": "kbC8juhA2Bc",
+    "title": "Dr. Katz; Professional Therapist :: S04E08 :: Closets :: 1440p"
+  },
+  {
+    "id": "HzbHQW3DmeI",
+    "title": "Dr. Katz; Professional Therapist :: S04E09 :: Wild Weekend :: 1440p"
+  },
+  {
+    "id": "Eg1OzvZlzhk",
+    "title": "Dr. Katz; Professional Therapist :: S04E10 :: Chopper :: 1440p"
+  },
+  {
+    "id": "Q6VBg0p7Gh8",
+    "title": "Dr. Katz; Professional Therapist :: S04E11 :: Alibi :: 1440p"
+  },
+  {
+    "id": "63oy2wow5xI",
+    "title": "Dr. Katz; Professional Therapist :: S04E12 :: Ben Centennial :: 1440p"
+  },
+  {
+    "id": "rVm4cGUFxbY",
+    "title": "Dr. Katz; Professional Therapist :: S04E13 :: Undercover Ben :: 1440p"
+  },
+  {
+    "id": "vOLoWXB3L2o",
+    "title": "Dr. Katz; Professional Therapist :: S05E01 :: Fanny Pack :: 1440p"
+  },
+  {
+    "id": "rZNcHp8m0-g",
+    "title": "Dr. Katz; Professional Therapist :: S05E02 :: Movies :: 1440p"
+  },
+  {
+    "id": "2unkzNDa6f0",
+    "title": "Dr. Katz; Professional Therapist :: S05E03 :: Old Man :: 1440p"
+  },
+  {
+    "id": "AB51g37UAZE",
+    "title": "Dr. Katz; Professional Therapist :: S05E04 :: Phone Luv :: 1440p"
+  },
+  {
+    "id": "Q96Js7G1rFk",
+    "title": "Dr. Katz; Professional Therapist :: S05E05 :: Metaphors :: 1440p"
+  },
+  {
+    "id": "Y3OqWHQUt5I",
+    "title": "Dr. Katz; Professional Therapist :: S05E06 :: Chain Letter :: 1440p"
+  },
+  {
+    "id": "5323On7yldQ",
+    "title": "Dr. Katz; Professional Therapist :: S05E07 :: Babysitting Ben :: 1440p"
+  },
+  {
+    "id": "quwXRpxTfUE",
+    "title": "Dr. Katz; Professional Therapist :: S05E08 :: Miles Away :: 1440p"
+  },
+  {
+    "id": "KSzWocCrbOk",
+    "title": "Dr. Katz; Professional Therapist :: S05E09 :: London Broil :: 1440p"
+  },
+  {
+    "id": "xVtdF8XMiMs",
+    "title": "Dr. Katz; Professional Therapist :: S05E10 :: Feng Shui :: 1440p"
+  },
+  {
+    "id": "EwhiYNnTQSA",
+    "title": "Dr. Katz; Professional Therapist :: S05E11 :: Alderman :: 1440p"
+  },
+  {
+    "id": "T2O_uUuyvlo",
+    "title": "Dr. Katz; Professional Therapist :: S05E12 :: Ticket :: 1440p"
+  },
+  {
+    "id": "yQPMt07S3lo",
+    "title": "Dr. Katz; Professional Therapist :: S05E13 :: Paranoia :: 1440p"
+  },
+  {
+    "id": "iNaUhFI6Kyo",
+    "title": "Dr. Katz; Professional Therapist :: S05E14 :: Waltz :: 1440p"
+  },
+  {
+    "id": "KkYs8bDE6ew",
+    "title": "Dr. Katz; Professional Therapist :: S05E15 :: Anniversary :: 1440p"
+  },
+  {
+    "id": "oqsVbAKsi4k",
+    "title": "Dr. Katz; Professional Therapist :: S05E16 :: Community Theater :: 1440p"
+  },
+  {
+    "id": "LCHFHOonXAs",
+    "title": "Dr. Katz; Professional Therapist :: S05E17 :: Ping Pong :: 1440p"
+  },
+  {
+    "id": "HlUpUVgxmi4",
+    "title": "Dr. Katz; Professional Therapist :: S05E18 :: Thanksgiving :: 1440p"
+  },
+  {
+    "id": "820FXnjMe0Y",
+    "title": "Dr. Katz; Professional Therapist :: S06E01 :: Sissy Boy :: 1440p"
+  },
+  {
+    "id": "_AHiwE_3WIo",
+    "title": "Dr. Katz; Professional Therapist :: S06E02 :: Pullman Square :: 1440p"
+  },
+  {
+    "id": "2wLZLBZYzjY",
+    "title": "Dr. Katz; Professional Therapist :: S06E03 :: Wisdom Teeth :: 1440p"
+  },
+  {
+    "id": "3OmLv4D1vQM",
+    "title": "Dr. Katz; Professional Therapist :: S06E04 :: Past Lives :: 1440p"
+  },
+  {
+    "id": "pTBkTQcNi78",
+    "title": "Dr. Katz; Professional Therapist :: S06E05 :: Ben's Partay :: 1440p"
+  },
+  {
+    "id": "2lQnrYdFe4A",
+    "title": "Dr. Katz; Professional Therapist :: S06E06 :: Used Car :: 1440p"
+  },
+  {
+    "id": "wroDPiVrzyA",
+    "title": "Dr. Katz; Professional Therapist :: S06E07 :: Ball and Chain :: 1440p"
+  },
+  {
+    "id": "jjIfzPecL0Y",
+    "title": "Dr. Katz; Professional Therapist :: S06E08 :: Snow Day :: 1440p"
+  },
+  {
+    "id": "IhDjxzTiVho",
+    "title": "Dr. Katz; Professional Therapist :: S06E09 :: Garden :: 1440p"
+  },
+  {
+    "id": "WOYw-ZwIPS0",
+    "title": "Dr. Katz; Professional Therapist :: S06E10 :: Big TV :: 1440p"
+  },
+  {
+    "id": "KIZrdjqq7YQ",
+    "title": "Dr. Katz; Professional Therapist :: S06E11 :: Vow of Silence :: 1440p"
+  },
+  {
+    "id": "rEClVvNunHw",
+    "title": "Dr. Katz; Professional Therapist :: S06E12 :: You're Belinda :: 1440p"
+  },
+  {
+    "id": "1Djni30HzYQ",
+    "title": "Dr. Katz; Professional Therapist :: S06E13 :: Radio Katz :: 1440p"
+  },
+  {
+    "id": "AaHbJCqTAaM",
+    "title": "Dr. Katz; Professional Therapist :: S06E14 :: Expert Witness :: 1440p"
+  },
+  {
+    "id": "X3uECqnfxHA",
+    "title": "Dr. Katz; Professional Therapist :: S06E15 :: Walk for Hunger :: 1440p"
+  },
+  {
+    "id": "_KdyHMarCqc",
+    "title": "Dr. Katz; Professional Therapist :: S06E16 :: Bakery Ben :: 1440p"
+  },
+  {
+    "id": "dH5-VW2YOCY",
+    "title": "Dr. Katz; Professional Therapist :: S06E17 :: Uncle Nothing :: 1440p"
+  },
+  {
+    "id": "LcUHNSh0M48",
+    "title": "Dr. Katz; Professional Therapist :: S06E18 :: Lerapy :: 1440p"
+  }
+];
